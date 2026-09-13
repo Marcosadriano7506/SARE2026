a81bbc8bdccefe935fd55bfb9f6db2026ea05f78
Marcosadriano7506 <marquinhos0675@gmail.com>
2026-09-13 07:46:15 -0300

    feat: add app/coordinator/forms.py

---
+++ app/coordinator/forms.py
@@ -0,0 +1,13 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length
+
+
+class ApplicatorForm(FlaskForm):
+    name = StringField("Nome", validators=[DataRequired(), Length(max=160)])
+    job_title = StringField(
+        "Função na Secretaria", validators=[DataRequired(), Length(max=160)]
+    )
+    username = StringField("Login", validators=[DataRequired(), Length(min=3, max=80)])
+    password = PasswordField("Senha", validators=[DataRequired(), Length(min=8, max=128)])
+    submit = SubmitField("Cadastrar aplicador")
