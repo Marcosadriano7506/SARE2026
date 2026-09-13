e1fc1de930dd46c67d25ed8cb845743e8d478821
Marcosadriano7506 <marquinhos0675@gmail.com>
2026-09-13 09:09:00 -0300

    feat: add application reassignment form

--- app/coordinator/application_forms.py
+++ app/coordinator/application_forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, TextAreaField
+from wtforms import SelectField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 
@@ -9,3 +9,16 @@ class ReopenClassForm(FlaskForm):
         validators=[DataRequired(), Length(min=5, max=500)],
     )
     submit = SubmitField("Reabrir turma")
+
+
+class ApplicationAssignmentForm(FlaskForm):
+    applicator_id = SelectField(
+        "Aplicador responsável",
+        coerce=int,
+        validators=[DataRequired()],
+    )
+    reason = TextAreaField(
+        "Motivo da alteração",
+        validators=[DataRequired(), Length(min=5, max=500)],
+    )
+    submit = SubmitField("Atualizar responsável")
